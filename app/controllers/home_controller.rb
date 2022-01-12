17aa848bd3e185335392d159d594cb9757b4e36e
rohit <rp0113948@gmail.com>
2022-01-12 20:18:59 +0530

    first commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,5 @@
+class HomeController < ApplicationController
+  def index
+    # layout :author
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  devise_for :authors
+  # get 'home/index'
+  root to: "home#index"
+  scope module: 'authors' do 
+    resources :posts
+  end
+end
+
